cce2732915b1bd5b41ac1f888dedded34b608ef4
Abhishekpatil2000 <115855289+Abhishekpatil2000@users.noreply.github.com>
2024-09-27 12:42:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //your JS code here. If required.
-document.getElementById('output')
 let Mndata= new Promise(function(resolve,reject) {
 	resolve([1,2,3,4]);
 });
@@ -10,24 +9,25 @@ Mndata.then((array)=>{
 			store.push(x);
 	
 		}
-		setTimeout(()=>{
+	}
+	setTimeout(()=>{
 		
 	},1000);
-		
-	}
 	
 	return store;
-});
+})
 .then((array)=>{
 	const mul=[];
 	for(let x of array){
-		mul.push(array[x]*2);
+		mul.push(x*2);
 	
 	}
 	setTimeout(()=>{
 	},2000);
 return mul;
 	})
-	
 
+})
+.then((array)=>{
+	document.getElementById('output').innerText= array.join(',');
 })
