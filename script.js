ba3f054ec3fcc3c8067596212f51d314fa0cb29f
Abhishekpatil2000 <115855289+Abhishekpatil2000@users.noreply.github.com>
2024-09-27 12:08:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,22 @@ let Mndata= new Promise(function(resolve,reject) {
 	resolve([1,2,3,4]);
 });
 Mndata.then((array)=>{
-	setTimeout() 
-})
-.catch(()=>{
-	
+	const store=[];
+	for(let x of array){
+		if(x%2!=0){
+			array=store.push(x);
+		}
+	}
+
+	setTimeout(()=>{
+		return array;
+	},3000);
+});
+.then((array)=>{
+	array.reduce((accumulator,currentvalue)=>{
+		array=currentvalue*2;
+	})
+	setTimeout(()=>{
+		return array
+	},3000);
 })
